c4cc545509892475fe9eeff6fc81a49e18fbf2d5
Virola <virola.zhu@gmail.com>
2018-04-17 17:39:37 +0800

    update icons to single import

--- src/main.js
+++ src/main.js
@@ -6,7 +6,27 @@ import router from './router'
 import store from './store'
 
 // icons
-import 'vue-awesome/icons'
+import 'vue-awesome/icons/regular/bookmark'
+import 'vue-awesome/icons/play'
+import 'vue-awesome/icons/angle-left'
+import 'vue-awesome/icons/angle-right'
+import 'vue-awesome/icons/arrow-up'
+import 'vue-awesome/icons/search'
+import 'vue-awesome/icons/user'
+import 'vue-awesome/icons/map-marker'
+import 'vue-awesome/icons/thumbs-up'
+import 'vue-awesome/icons/star'
+import 'vue-awesome/icons/lock'
+import 'vue-awesome/icons/mobile-alt'
+import 'vue-awesome/icons/check'
+import 'vue-awesome/icons/regular/edit'
+import 'vue-awesome/icons/regular/user'
+import 'vue-awesome/icons/regular/star'
+import 'vue-awesome/icons/regular/comments'
+import 'vue-awesome/icons/brands/weibo'
+import 'vue-awesome/icons/brands/weixin'
+import 'vue-awesome/icons/brands/qq'
+
 import Icon from 'vue-awesome/components/Icon'
 
 // 公共组件
