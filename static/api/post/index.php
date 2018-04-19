0fba33efae05b528c548459710e541b122d54a29
Virola <virola.zhu@gmail.com>
2018-04-19 15:10:17 +0800

    update post api

--- static/api/post/index.php
+++ static/api/post/index.php
@@ -6,6 +6,12 @@ $type = $_GET['type'];
 
 $resp = array(
   'status' => 'ok',
-  'data' => array() 
+  'data' => array(
+    'userInfo' => array(
+      'user_id'=> 530,
+      'username'=> 'virola'
+    )
+  ) 
 );
+
 echo json_encode($resp);
